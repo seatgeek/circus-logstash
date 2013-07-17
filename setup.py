df3659e80c97887caf2e6a29970049cd49953482
Philip Cristiano <philipcristiano@gmail.com>
2013-07-17 14:56:00 -0400

    v0.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 def run_setup():
     setup(
         name='circus-logstash',
-        version='0.0.2',
+        version='0.0.3',
         description='A Logstash shipper for Circus',
         keywords = 'logstash circus',
         url='https://github.com/seatgeek/circus-logstash',
